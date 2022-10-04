b1ba979d1ec5743330997079e134ee672f7a879a
Dakota Kelley <dkelley@phdata.io>
2022-10-04 20:47:03 +0000

    Fix swap_database macro

--- macros/swap_database.sql
+++ macros/swap_database.sql
@@ -2,7 +2,4 @@
     {% set sql = 'ALTER DATABASE prod_coalesce_demo SWAP WITH test_coalesce_demo' %}
     {% do run_query(sql) %}
     {{ log("database swapped", info=True) }}
-    {% set sql = 'CREATE OR REPLACE SCHEMA test_coalesce_demo.artifacts CLONE prod_coalesce_demo.artifacts' %}
-    {% do run_query(sql) %}
-    {{ log("Artifacts Cloned", info=True) }}
 {% endmacro %}
