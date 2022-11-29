2ee522802ae6c687158300d3b7982a51c215eed2
Dakota Kelley <dkelley@phdata.io>
2022-11-29 02:44:07 +0000

    Update to clean the data

--- models/clean/cln_investigator_awards.sql
+++ models/clean/cln_investigator_awards.sql
@@ -1,4 +1,13 @@
-WITH investigator_awards AS (
+WITH pre_clean AS (
+  SELECT award_id,
+           email_id,
+           REPLACE(start_date, '"', '') AS start_date,
+           REPLACE(end_date, '"', '') AS end_date,
+           role_code
+    FROM {{ source('raw_grants', 'investigator_awards') }}
+),
+
+investigator_awards AS (
     SELECT award_id,
            {{ dbt_utils.surrogate_key(['award_id']) }} AS award_key,
            email_id,
@@ -6,7 +15,7 @@ WITH investigator_awards AS (
            DECODE(start_date, '', NULL, TO_DATE(start_date, 'MM/DD/YYYY')) AS start_date,
            DECODE(end_date, '', NULL, TO_DATE(end_date, 'MM/DD/YYYY')) AS end_date,
            role_code
-      FROM {{ source('raw_grants', 'investigator_awards') }}
+      FROM pre_clean
      WHERE award_id IN (SELECT award_id
                           FROM {{ source('raw_grants', 'awards') }} ) AND
            email_id IN (SELECT email_id
