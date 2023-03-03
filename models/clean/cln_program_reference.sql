bfe520c004ee291813b68359a0289ec8d5665ff0
Dakota Kelley <dkelley@phdata.io>
2023-03-03 14:56:20 +0000

    add source origination

--- models/clean/cln_program_reference.sql
+++ models/clean/cln_program_reference.sql
@@ -1,11 +1,13 @@
 WITH program_reference AS (
     SELECT code,
            {{ dbt_utils.surrogate_key(['code']) }} AS program_reference_key,
-           text AS desc
+           text AS desc,
+           'grants' AS load_data
       FROM {{ source('raw_grants', 'program_reference') }}
 )
 
 SELECT code,
        program_reference_key,
-       desc
+       desc,
+       load_data
   FROM program_reference
