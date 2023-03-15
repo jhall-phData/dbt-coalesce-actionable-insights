fb574187a257ac11f4935e92b13168a428d52fa2
Dakota Kelley <dkelley@phdata.io>
2023-03-15 15:10:25 +0000

    Add random field to foa_info

--- models/clean/cln_foa_info.sql
+++ models/clean/cln_foa_info.sql
@@ -1,11 +1,13 @@
 WITH foa_info AS (
     SELECT code,
            {{ dbt_utils.surrogate_key(['code']) }} AS foa_info_key,
-           name
+           name,
+           'random_field' AS random_field
       FROM {{ source('raw_grants', 'foa_info') }}
 )
 
 SELECT code,
        foa_info_key,
-       name
+       name,
+       random_field
   FROM foa_info
